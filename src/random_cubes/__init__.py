a7d3d3e7d74496860b0c733c9425587f89304c4a
Florian REY <florianrey.info@gmail.com>
2026-01-05 16:52:10 +0100

    ruffed

--- src/random_cubes/__init__.py
+++ src/random_cubes/__init__.py
@@ -11,8 +11,9 @@
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-import bpy
 import random
+
+import bpy
 
 from . import auto_load
 
